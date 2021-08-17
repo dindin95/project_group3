5b82454a237a5cad27f26a2cdb769f20506552e2
jh_lee <tale2011@naver.com>
2021-08-17 12:33:10 +0900

    MemberDTO

---
+++ src/com/g3/dto/MemberDTO.java
@@ -0,0 +1,50 @@
+package com.g3.dto;
+
+public class MemberDTO {
+	
+	private String m_id;
+	private String m_name;
+	private String m_pwd;
+	private String m_phone;
+	private String m_date;
+	private String m_level;
+
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public String getM_name() {
+		return m_name;
+	}
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+	public String getM_pwd() {
+		return m_pwd;
+	}
+	public void setM_pwd(String m_pwd) {
+		this.m_pwd = m_pwd;
+	}
+	public String getM_phone() {
+		return m_phone;
+	}
+	public void setM_phone(String m_phone) {
+		this.m_phone = m_phone;
+	}
+	public String getM_date() {
+		return m_date;
+	}
+	public void setM_date(String m_date) {
+		this.m_date = m_date;
+	}
+	public String getM_level() {
+		return m_level;
+	}
+	public void setM_level(String m_level) {
+		this.m_level = m_level;
+	}
+
+	
+}
